4a8eab87afdfb1eaabf1c352632050a35e97b419
natasilva01 <natalia.silva@mercadolivre.com>
2021-09-27 23:46:17 -0300

    atualização integração docker

--- src/main/java/com/impacta/microservices/investimento/InvestimentoApplication.java
+++ src/main/java/com/impacta/microservices/investimento/InvestimentoApplication.java
@@ -2,9 +2,12 @@ package com.impacta.microservices.investimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class InvestimentoApplication {
 
--- src/main/java/com/impacta/microservices/investimento/controller/InvestimentoController.java
+++ src/main/java/com/impacta/microservices/investimento/controller/InvestimentoController.java
@@ -3,6 +3,7 @@ package com.impacta.microservices.investimento.controller;
 import com.impacta.microservices.investimento.domain.Investimento;
 import com.impacta.microservices.investimento.service.InvestimentoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +14,16 @@ import java.net.UnknownHostException;
 @RequestMapping("/conta/investimento")
 public class InvestimentoController {
 
+    @Autowired
     private final InvestimentoService investimentoService;
 
     public InvestimentoController(InvestimentoService investimentoService) {
         this.investimentoService = investimentoService;
+    }
+
+    @GetMapping("/status_aplicacao")
+    public String checarStatusAplicacao(){
+        return "Aplicação online";
     }
 
     @PostMapping
